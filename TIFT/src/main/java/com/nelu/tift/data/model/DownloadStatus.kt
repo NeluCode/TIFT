011eb43f64b944599bd0d98e47e1912e5fb55001
alim <sourav.0.alim@gmail.com>
2024-04-08 01:07:14 +0600

    Fi issue with download status

--- TIFT/src/main/java/com/nelu/tift/data/model/DownloadStatus.kt
+++ TIFT/src/main/java/com/nelu/tift/data/model/DownloadStatus.kt
@@ -6,9 +6,12 @@ import androidx.annotation.Keep
 @Keep
 sealed class DownloadStatus {
 
+    @Keep
     data class Progress(val progress: Int) : DownloadStatus()
 
+    @Keep
     object Completed : DownloadStatus()
 
+    @Keep
     data class Error(val message: String) : DownloadStatus()
 }
